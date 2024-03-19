8d67a9b0e63b0d62a2d00bd2f82adee72c18a66e
Namsraijav <Namsraijav.d@databank.mn>
2024-03-19 17:50:59 +0800

    diff check

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,7 @@
+""" django command to wait for database to be available. """
+
+from django.core.management.base import BaseCommand
+"""Djange command to wait for database"""
+
+def handle(self, *args, **options):
+    pass
